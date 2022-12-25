0b3ecc02fda857fae3c6f1810fec92e31d377b96
GuilhermeRaposo <dev@graposo.me>
2022-12-25 14:27:48 -0700

    Add option to add user without signing out

--- steam_account_switcher/main.py
+++ steam_account_switcher/main.py
@@ -10,9 +10,12 @@ users = load_users()
 user_names = get_usernames(users)
 
 # Show CLI options
+user_names.append("Add User")
 title = "Please choose the account you want to login with: "
 options = user_names
 selected_username, index = pick(options, title, indicator='=>', default_index=0)
+
+if (selected_username == "Add User"): selected_username = "None"
 
 selected_userid = get_userid_from_username(users, selected_username)
 current_userid = get_current_logged_userid(users)
