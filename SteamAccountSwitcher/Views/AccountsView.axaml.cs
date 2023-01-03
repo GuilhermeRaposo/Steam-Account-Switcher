e25fdc7a0128199c0ea526b7f9a53d1f072f5c23
GuilhermeRaposo <dev@graposo.me>
2023-01-02 19:21:40 -0700

    Improve UI

--- SteamAccountSwitcher/Views/AccountsView.axaml.cs
+++ SteamAccountSwitcher/Views/AccountsView.axaml.cs
@@ -4,15 +4,8 @@ using System;
 
 namespace SteamAccountSwitcher.Views {
     public partial class AccountsView : UserControl {
-        private string Selected { get; set; }
-
         public AccountsView() {
             InitializeComponent();
-        }
-
-        public void AccountSelected(object sender, RoutedEventArgs e) {
-            Selected = e.Source.ToString();
-            Console.WriteLine(123);
         }
     }
 }
